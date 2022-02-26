64d84f0e52577e37bdca446870520cc742e34df4
Kevin Jan <kevinjanbarluado2@gmail.com>
2022-02-27 05:29:56 +0800

    Current path if not declared

--- changeext.js
+++ changeext.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 const myOptions = {
-    'path': { alias: 'p', type: 'string', describe: 'Path of the folder' },
+    'path': { alias: 'p', type: 'string', describe: 'Path of the folder (leave blank to get current directory)' },
     'from': { alias: 'f', type: 'string', describe: 'Search extensions inside the chosen folder' },
-    'to': { alias: 't', type: 'string', describe: 'Update Files with the new extension' }
+    'to': { alias: 't', type: 'string', describe: 'Update files with the new extension' }
 
 };
-const argv = require('yargs').options(myOptions).demandOption(['path', 'from', 'to']).help().parse();
-const location = argv.path;
+const argv = require('yargs').version('1.1.0').options(myOptions).demandOption(['from', 'to']).help().parse();
+
+const location = argv.path || __dirname;
+console.log(location)
 const readFolder = fs.readdirSync(location);
 const fromExt = argv.from;
 const toExt = argv.to;
