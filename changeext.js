45143db420ef378bc4be9e613ecdd0d1b7db92ae
Kevin Jan <kevinjanbarluado2@gmail.com>
2022-02-27 05:15:46 +0800

    Updated Commands

--- changeext.js
+++ changeext.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const argv = require('yargs').demandOption(['path', 'from', 'to'], 'Path = directory\nFrom = current extension\nTo = update to this new extension').parse();
+const myOptions = {
+    'path': { alias: 'p', type: 'string', describe: 'Path of the folder' },
+    'from': { alias: 'f', type: 'string', describe: 'Search extensions inside the chosen folder' },
+    'to': { alias: 't', type: 'string', describe: 'Update Files with the new extension' }
+
+};
+const argv = require('yargs').options(myOptions).demandOption(['path', 'from', 'to']).help().parse();
 const location = argv.path;
 const readFolder = fs.readdirSync(location);
 const fromExt = argv.from;
